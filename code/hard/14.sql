50fb4b5d9b844fea50be5035abb2cbb5cdaa8d0e
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-05-24 10:34:26 +0200

    fix a typo in answer 14

--- code/hard/14.sql
+++ code/hard/14.sql
@@ -11,8 +11,7 @@ union
 select "1000- 9999" as "RANGE", count(SalesOrderID) as "Num Orders", sum(SubTotal) as "Total Value"
 from SalesOrderHeader
 where SubTotal between 1000 and 9999
-
 union
 select "10000-" as "RANGE", count(SalesOrderID) as "Num Orders", sum(SubTotal) as "Total Value"
 from SalesOrderHeader
-where SubTotal >= 1e5
+where SubTotal >= 1e4
