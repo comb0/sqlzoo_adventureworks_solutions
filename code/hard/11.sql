5d92aa023426b7c09b3c9a6e81b5dea926f01f9b
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-05-24 09:53:52 +0200

    attempt to correct answer to question 11

--- code/hard/11.sql
+++ code/hard/11.sql
@@ -1,7 +1,8 @@
-select moa.AddressLine1 as 'Main Office Address', sa.AddressLine1 as 'Shipping Address'
-from CustomerAW
+select CompanyName,moa.AddressLine1 as 'Main Office Address', sa.AddressLine1 as 'Shipping Address'
+from CustomerAW as caw
 join CustomerAddress as ca using (CustomerID)
 join CustomerAddress as cb using (CustomerID)
 join Address as moa on (ca.AddressID = moa.AddressID and ca.AddressType='Main Office')
 left join Address as sa on (cb.AddressID = sa.AddressID and cb.AddressType='Shipping')
 where moa.City = 'Dallas'
+group by caw.CustomerID
