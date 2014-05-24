9cf3a1db7bce861bea3660468b3a3c6cc9cd219e
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-05-24 09:42:32 +0200

    attempt to solve question 11

--- code/hard/11.sql
+++ code/hard/11.sql
@@ -1,1 +1,7 @@
-/* TODO */
+select moa.AddressLine1 as 'Main Office Address', sa.AddressLine1 as 'Shipping Address'
+from CustomerAW
+join CustomerAddress as ca using (CustomerID)
+join CustomerAddress as cb using (CustomerID)
+join Address as moa on (ca.AddressID = moa.AddressID and ca.AddressType='Main Office')
+left join Address as sa on (cb.AddressID = sa.AddressID and cb.AddressType='Shipping')
+where moa.City = 'Dallas'
