1cd41bd34a9cce17e45831f842d032ad1b184584
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-05-24 09:42:21 +0200

    more formatting

--- code/easy/4.sql
+++ code/easy/4.sql
@@ -1,7 +1,9 @@
 select CompanyName
 from CustomerAW join
-(select CustomerID as ci,sum(SubTotal)+sum(Freight)+sum(TaxAmt) as t
-from SalesOrderHeader
-group by CustomerID) as totals
+(
+	select CustomerID as ci, sum(SubTotal)+sum(Freight)+sum(TaxAmt) as t
+	from SalesOrderHeader
+	group by CustomerID
+) as totals
 on(totals.ci = CustomerAW.CustomerID)
 where totals.t > 1e5
